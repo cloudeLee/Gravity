724f1e55fa752f9b044cf09632ce82a5df4d7b6d
cloude.lee <cloude.leez@gmail.com>
2014-12-27 01:03:38 +0900

    * cocos2d-x version upgrade - [3.3]

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -1,5 +1,5 @@
 #include "AppDelegate.h"
-#include "scene/ingamescene/InGameScene.h"
+#include "InGameScene.h"
 
 USING_NS_CC;
 
@@ -11,77 +11,52 @@ AppDelegate::~AppDelegate()
 {
 }
 
+//if you want a different context,just modify the value of glContextAttrs
+//it will takes effect on all platforms
+void AppDelegate::initGLContextAttrs()
+{
+    //set OpenGL context attributions,now can only set six attributions:
+    //red,green,blue,alpha,depth,stencil
+    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
+
+    GLView::setGLContextAttrs(glContextAttrs);
+}
+
 bool AppDelegate::applicationDidFinishLaunching() {
     // initialize director
     auto director = Director::getInstance();
-	auto glview = director->getOpenGLView();
-	if (!glview) {
-		glview = GLView::create("My Game");
+    auto glview = director->getOpenGLView();
+    if(!glview) {
+        glview = GLViewImpl::create("My Game");
+
+#if SEAFT_MAP_EDITOR
+		glview->setFrameSize(1600, 800);
+#else
 		glview->setFrameSize(800, 1280);
 		glview->setFrameZoomFactor(0.5);
-		director->setOpenGLView(glview);
-	}
+#endif
+        director->setOpenGLView(glview);
+    }
 
-	// Set the design resolution
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT) || (CC_TARGET_PLATFORM == CC_PLATFORM_WP8)
-	glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, kResolutionShowAll);
+#if SEAFT_MAP_EDITOR
+	glview->setDesignResolutionSize(4000, 2400, ResolutionPolicy::EXACT_FIT);
 #else
-	//pEGLView->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, kResolutionNoBorder);
-	glview->setDesignResolutionSize(1080, 1920, kResolutionExactFit);
+	glview->setDesignResolutionSize(1080, 1920, ResolutionPolicy::EXACT_FIT);
 #endif
 
-	// turn on display FPS
-	director->setDisplayStats(true);
+    // turn on display FPS
+    director->setDisplayStats(true);
 
-	auto fileUtils = FileUtils::getInstance();
-	auto screenSize = glview->getFrameSize();
-	std::vector<std::string> resDirOrders;
+    // set FPS. the default value is 1.0/60 if you don't call this
+    director->setAnimationInterval(1.0 / 60);
 
-// 	int screenWidth = screenSize.width <= screenSize.height ? screenSize.width : screenSize.height;
-// 
-// 	if (screenWidth >= 1600)
-// 	{
-// 		resDirOrders.push_back("res/ipad");
-// 		resDirOrders.push_back("res/iphonehd5");
-// 		resDirOrders.push_back("res/iphonehd");
-// 		resDirOrders.push_back("res/iphone");
-// 
-// 		glview->setDesignResolutionSize(1600, 2560, ResolutionPolicy::NO_BORDER);
-// 	}
-// 	else if (screenWidth >= 1080)
-// 	{
-// 		resDirOrders.push_back("res/iphonehd5");
-// 		resDirOrders.push_back("res/iphonehd");
-// 		resDirOrders.push_back("res/iphone");
-// 
-// 		glview->setDesignResolutionSize(1080, 1920, ResolutionPolicy::NO_BORDER);
-// 	}
-// 	else if (screenWidth >= 800)
-// 	{
-// 		resDirOrders.push_back("res/iphonehd");
-// 		resDirOrders.push_back("res/iphone");
-// 
-// 		glview->setDesignResolutionSize(800, 1280, ResolutionPolicy::NO_BORDER);
-// 	}
-// 	else
-// 	{
-// 		resDirOrders.push_back("res/iphone");
-// 
-// 		glview->setDesignResolutionSize(480, 800, ResolutionPolicy::NO_BORDER);
-// 	}
-
-	fileUtils->setSearchPaths(resDirOrders);
-
-	// set FPS. the default value is 1.0/60 if you don't call this
-	director->setAnimationInterval(1.0 / 60);
-
-	// create a scene. it's an autorelease object
+    // create a scene. it's an autorelease object
 	auto scene = InGameScene::createScene();
 
-	// run
-	director->runWithScene(scene);
+    // run
+    director->runWithScene(scene);
 
-	return true;
+    return true;
 }
 
 // This function will be called when the app is inactive. When comes a phone call,it's be invoked too
